caf31c076904de3f9f9fdbdf434ca4531261c27d
Lina-Romero <linafernandaromeroc@gmail.com>
2026-08-11 17:22:20 -0500

    chore: correcciones de rutas

--- Backend/routes/input_output/input_output.js
+++ Backend/routes/input_output/input_output.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import router from express.Router();
-import { obtenerRegistros, crearRegistro } from require('../controller/input_outputcontroller');
+import { obtenerRegistros, crearRegistro } from require('../controller/input_output/input_outputcontroller');
 
 router.get('/', obtenerRegistros);
 router.post('/', crearRegistro);
--- Backend/routes/quota/quota.js
+++ Backend/routes/quota/quota.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import router from express.Router();
-import { obtenerCupos, asignarCupo } from require('../controller/quotacontroller');
+import { obtenerCupos, asignarCupo } from require('../controller/quota/quotacontroller');
 
 router.get('/', obtenerCupos);
 router.post('/', asignarCupo);

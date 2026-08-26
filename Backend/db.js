f1217b9b1f45ad73c61f0189219c9b85d6dbe6d4
Steveen Cortes <steveenprofessional@gmail.com>
2026-08-26 15:00:15 -0500

    fix: agrega soporte para DB_PORT; limpia .env.example de contenido que no era configuración

--- Backend/db.js
+++ Backend/db.js
@@ -3,9 +3,11 @@ import dotenv from 'dotenv';
 
 const pool = mysql.createPool ({
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'SECSS_db',
+    charset: 'utf8mb4',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
